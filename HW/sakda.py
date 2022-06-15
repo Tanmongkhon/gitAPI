b7a072f1b0af1f2a08ec03d3ae514430e8aa88e9
Sakda Yokhawat <kaodentay@gmail.com>
2022-06-15 15:25:43 +0700

    new

--- HW/sakda.py
+++ HW/sakda.py
@@ -1,18 +1,6 @@
-def insert_data(name, hw_name, status,value):
-    mydb = con()
-    mycursor = mydb.cursor(dictionary=True)
-    sql = "INSERT INTO hw (name, hw_name, status,value) VALUE ('{}','{}', '{}',{})".format(
-        name, hw_name, status,value
-    )
-    mycursor.execute(sql)
-    mydb.commit()
-    ID = mycursor.fetchall()
-    return ID
-def delete_hw_maxData(data):
-    mydb = con()
-    mycursor = mydb.cursor(dictionary=True)
-    sql = "DELETE FROM hw WHERE id = (SELECT {}(id) FROM hw)".format(
-        data
-    )
-    mycursor.execute(sql)
-    mydb.commit()
+mycursor.execute(sql)
+mydb.commit()
+mycursor.close()
+mydb.close()
+ID = mycursor.lastrowid
+return ID
